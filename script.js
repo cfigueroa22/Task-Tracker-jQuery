5ac71d25167363fff04402c180f07dc02bf2c6ba
Carlos Figueroa <carlosfigueroa@carloss-mbp.myfiosgateway.com>
2023-09-11 21:50:01 -0400

    add task functionality and delete

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-$("document").ready(function () {});
+$(document).ready(function () {
+  $("#button").click(function () {
+    var task = $("input[name=list-item]").val();
+    $("#tasks").append("<li>" + task + "</li>");
+  });
+
+  $(document).on("dblclick", "li", function () {
+    $(this).toggleClass("strike").fadeOut("slow");
+  });
+
+  $("input").focus(function () {
+    $(this).val(" ");
+  });
+});
